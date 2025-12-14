b34a5c52671be32b6966bcf2137dd30feb25d30b
Kesero <90833008+k3sero@users.noreply.github.com>
2025-12-14 16:51:11 +0100

    Delete Competiciones_Nacionales_Writeups/2025/Talent4Cyber2025/Misc/Deadlock/solver.py

--- Competiciones_Nacionales_Writeups/2025/Talent4Cyber2025/Misc/Deadlock/solver.py
+++
@@ -1,3 +0,0 @@
-# Definir una variable a: a=eⅺt utilizando el carácter unicode ⅺ, para cumplir la condición de 5 caracteres.
-# Sobreescribir id=a con un espacio al final para cumplir la condición. 
-# exit(str) exfiltra el contenido del string.
